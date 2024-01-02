2175fa700e5bdfc7d1a4396111859ba7998d845b
Danila <danil.akulov.95@mail.ru>
2024-01-02 23:31:23 +0500

    summary

--- bot.py
+++ bot.py
@@ -22,31 +22,36 @@ def start(message):
 def next_record(message):
     bot.send_message(message.chat.id, 'Предоставьте следующим сообщением запись.')
 
-
-    @bot.message_handler(content_types=['voice', 'text'])
+    @bot.message_handler(content_types=['voice'])
     def treatment(record):
-        file_info = bot.get_file(record.voice.file_id)
-        print(file_info)
-        file = requests.get('https://api.telegram.org/file/bot{0}/{1}'.format(token, file_info.file_path))
+        model_processing(bot.get_file(record.voice.file_id), record.chat.id)
 
-        with open('data/voice.wav', 'wb') as f:
-            f.write(file.content)
+    @bot.message_handler(content_types=['audio'])
+    def treatment(record):
+        model_processing(bot.get_file(record.audio.file_id), record.chat.id)
 
-        bot.send_message(record.chat.id, 'Обработка…')
 
-        resp = random.randint(1, 3)
-        normal = 'Аудиозапись содержит нормальную русскую речь.'
-        notNormal = 'Аудиофайл содержит ненормальную русскую речь.'
-        nothing = 'Не удалось проанализировать предоставленный файл.'
-        if resp == 1:
-            bot.send_message(record.chat.id, normal)
-        elif resp == 2:
-            bot.send_message(record.chat.id, notNormal)
-        else:
-            bot.send_message(record.chat.id, nothing)
+def model_processing(file_info, chat_id):
+    file = requests.get('https://api.telegram.org/file/bot{0}/{1}'.format(token, file_info.file_path))
 
-        bot.send_message(record.chat.id,
-                         'Для отправки ещё одного запроса с повторным или новым аудиофайлом нажмите "Предоставить запись".')
+    with open('data/voice.wav', 'wb') as f:
+        f.write(file.content)
+
+    bot.send_message(chat_id, 'Обработка…')
+
+    resp = random.randint(1, 3)
+    normal = 'Аудиозапись содержит нормальную русскую речь.'
+    notNormal = 'Аудиофайл содержит ненормальную русскую речь.'
+    nothing = 'Не удалось проанализировать предоставленный файл.'
+    if resp == 1:
+        bot.send_message(chat_id, normal)
+    elif resp == 2:
+        bot.send_message(chat_id, notNormal)
+    else:
+        bot.send_message(chat_id, nothing)
+
+    bot.send_message(chat_id,
+                     'Для отправки ещё одного запроса с повторным или новым голосовым сообщением нажмите "Предоставить запись".')
 
 
 bot.infinity_polling()
